055d886fb474ea4846b1b29dab4bc08910d0db2d
nexusbreathbond <terrancevaz9701@gmail.com>
2026-06-06 14:59:27 +0200

    Add file

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+pub mod bm25;
+pub mod chunking;
+pub mod digest;
+pub mod encoder;
+pub mod file_walker;
+pub mod filter;
+pub mod graph;
+pub mod index;
+pub mod outline;
+pub mod plan;
+pub mod ranking;
+pub mod search;
+pub mod stats;
+pub mod tokens;
+pub mod tree;
+pub mod types;
+pub mod utils;
+
+pub use graph::DependencyGraph;
+pub use index::SembleIndex;
+pub use types::{Chunk, IndexStats, SearchResult};
